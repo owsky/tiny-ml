8cc465b36b703fca71415d154e003e9c7069f9ba
Nicolò Bertocco <nick.bertocco@gmail.com>
2025-06-01 12:20:36 +0200

    updated build script for cloudflare compatibility

--- build.sh
+++ build.sh
@@ -2,5 +2,9 @@
 curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh
 chmod +x dotnet-install.sh
 ./dotnet-install.sh -c 8.0 -InstallDir ./dotnet
-./dotnet/dotnet --version
-./dotnet/dotnet publish -c Release --property:PublishDir=output 
+
+export PATH="$PWD/dotnet:$PATH"
+
+dotnet --version
+dotnet workload install wasm-tools
+dotnet publish -c Release -o output src/Web/Web.fsproj
